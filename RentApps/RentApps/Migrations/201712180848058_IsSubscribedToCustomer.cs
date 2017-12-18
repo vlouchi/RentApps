3fe1900c3c0091d58c071807ab91bbd6cc654164
IFT-PC\IFT <vlouchi@gmail.com>
2017-12-18 23:45:44 +0700

    Code First to Database; Insert value to MembershipType

---
+++ RentApps/RentApps/Migrations/201712180848058_IsSubscribedToCustomer.cs
@@ -0,0 +1,18 @@
+namespace RentApps.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IsSubscribedToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "isSubscribedToNewsletter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "isSubscribedToNewsletter");
+        }
+    }
+}
--- RentApps/RentApps/Models/Customer.cs
+++ RentApps/RentApps/Models/Customer.cs
@@ -9,6 +9,8 @@ namespace RentApps.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public bool isSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }

6ecae3a3bcee5e1d264cd28c88d6a9a806745f3b
IFT-PC\IFT <vlouchi@gmail.com>
2017-12-26 00:58:04 +0700

    add column "Name" to MembershipTypes database, Updating MembershipTypesdatabase

--- RentApps/RentApps/Controllers/CustomersController.cs
+++ RentApps/RentApps/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,19 +8,33 @@ using RentApps.Models;
 
 namespace RentApps.Controllers
 {
+   
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-            return View(customers);
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers); 
 
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
 
@@ -28,16 +43,6 @@ namespace RentApps.Controllers
         }
 
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Irba Fairuz Thufailah"},
-                new Customer {Id = 2, Name = "Sahna Melly Marselina"},
-                new Customer {Id = 3, Name = "Intan Raharni Wijaya"},
-                new Customer {Id = 4, Name = "Grace Duma Tambunan"}
-            };
-
-        }
+        
     }
 }
--- RentApps/RentApps/Models/MembershipType.cs
+++ RentApps/RentApps/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace RentApps.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        public String Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
         public byte DiscountRate { get; set; }
